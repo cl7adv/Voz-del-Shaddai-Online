d3a8f82a45f0330f0aeffb3aefcd5579e539c049
cl7adv <valdes1979alfredo@gmail.com>
2026-01-05 21:54:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const streamUrl = "TU_URL_DE_STREAMING_AQUI"; // Lee el paso 3
+const audio = new Audio(streamUrl);
+const playBtn = document.getElementById('playBtn');
 
+playBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        playBtn.innerText = "⏸";
+    } else {
+        audio.pause();
+        playBtn.innerText = "▶";
+    }
+});
+
+document.getElementById('volume').addEventListener('input', (e) => {
+    audio.volume = e.target.value;
+});
